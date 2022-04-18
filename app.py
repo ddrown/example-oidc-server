93ebe3c850e1ed00dc95addde63bc9ed37168f89
Daniel Drown <dan-android@drown.org>
2022-04-18 11:55:44 -0500

    note about SECRET_KEY

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from website.app import create_app
 
 
 app = create_app({
-    'SECRET_KEY': 'secret',
     'SQLALCHEMY_TRACK_MODIFICATIONS': False,
     'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite',
 })
